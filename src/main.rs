85487162b5770155838003c92f794586d56fd11b
dOtOb9 <dotob9@gmail.com>
2025-08-03 12:50:17 +0900

    add License, etc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use rapier2d::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    use nalgebra::{Isometry2, vector};
+
+    let iso = Isometry2::new(vector![1.0, 2.0], 0.5);
+
+    assert_eq!(iso.rotation.angle(), 0.5);
+    assert_eq!(iso.translation.vector.x, 1.0);
+    assert_eq!(iso.translation.vector.y, 2.0);
 }
